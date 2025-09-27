ef887207be635b5f1500f9ff6bef041584a1ab95
jahanzaib8-web <jahanzaibahmad126@gmail.com>
2025-09-27 06:50:47 +0000

    added console

---
+++ basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 134232;
+let accountEmail = "jahanzaib@google.com";
+var accountPassword = "12345";
+accountCity = "Daiwal";
+
+
+// accountId = 2 // not allowed
+accountEmail = "jeha@hs"
+accountPassword= "7e37w84"
+
+accountCity ="khushab"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+console.log(accountId);
+
