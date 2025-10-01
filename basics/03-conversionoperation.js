aa7f0bac98ab94d9a929f6cc439ae5ead39fe619
jahanzaib8-web <jahanzaibahmad126@gmail.com>
2025-10-01 05:42:04 +0000

    next video onconversationoperation

--- basics/03-conversionoperation.js
+++ basics/03-conversionoperation.js
@@ -1,7 +1,48 @@
+
 let score = "54abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+
+//console.log(typeof (score))
+
 let valueINnumber =Number(score) 
-console.log(typeof valueINnumber)
-console.log(valueINnumber)
+
+//console.log(typeof valueINnumber)
+
+//console.log(valueINnumber) 
+
+let someNumber = 33
+
+//let stringNumber = String(someNumber)
+
+//console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations **********************
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+// console.log(2+2);
+
+// console.log(2*3);
+
+// console.log(2-2);
+
+// console.log(2**3);
+
+// console.log(2/2)
+ let str1 = "Hello"
+ let str2 = " jahanzaib"
+
+ let str3 = str1 + str2
+ console.log(str3);
+ 
+ console.log("1" + 2);
+ console.log(1 + "2" );
+ console.log("1" + 2 + 2 );
+ console.log(1 + 2 + "2" );
+
+ let gamecounter = 300 
+ gamecounter++;
+ console.log(gamecounter);
+ 
