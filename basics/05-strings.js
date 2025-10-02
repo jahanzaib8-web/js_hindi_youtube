2f994f85ed4573e96a73cd6a2a13de2015748ed8
jahanzaib8-web <jahanzaibahmad126@gmail.com>
2025-10-02 07:29:58 +0000

    strings

---
+++ basics/05-strings.js
@@ -0,0 +1,16 @@
+const name = "jahanzaib"
+const repocount = 50
+// console.log(name + repocount + " value")
+
+console.log (`Hello my name is ${name} my repocount is ${repocount}`)
+
+const gamechange = new String ('jahanzaib')
+  
+console.log(gamechange[0]);
+console.log(gamechange.__proto__);
+console.log(gamechange.length);
+console.log(gamechange.charAt(2));
+console.log(gamechange.indexOf('h'));
+console.log(gamechange.toUpperCase());
+
+
