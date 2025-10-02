c52717decdf41c7cd732888b6b8e9ad55d8462de
jahanzaib8-web <jahanzaibahmad126@gmail.com>
2025-10-02 05:49:24 +0000

    Memory stack and heap

--- basics/datatypes-summary.js
+++ basics/datatypes-summary.js
@@ -34,3 +34,26 @@ const myFunction = function(){
 
 console.log(typeof anotherId);
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack(premative) , Heap(non-premative)
+
+let myyoutubename = "jahanzaibahmad"
+
+let anothername = myyoutubename
+
+anothername = "chaiorcode"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "usergoogl@gmial.com",
+    upi: "yt@67"
+}
+
+let userTwo = userOne;
+userTwo.email = "jahanziab@google.com";
+
+console.log(userOne.email); 
+console.log(userTwo.email);
