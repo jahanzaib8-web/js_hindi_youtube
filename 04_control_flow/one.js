2507d079228754cc2e687f5ea6c3669843a680a1
jahanzaib8-web <jahanzaibahmad126@gmail.com>
2025-10-17 05:32:18 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+
+// const  score = 200 
+// if (score >100) {
+//     const power = fly
+//     console.log(`user power ${power}`) 
+// }
+
+// console.log(`user power ${power}`) 
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+const isUserloggedIn = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinformemail = true
+
+if (isUserloggedIn && debitcard && 2==3) {
+    console.log("Allow to buy course")
+}
+
+if (loggedinfromgoogle || loggedinformemail) {
+    console.log("User logged in")
+}
